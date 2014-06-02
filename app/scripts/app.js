23e239572e24ab58e502d34d2a49f52f58fbb617
Austin Hesterly <austin.hesterly@gmail.com>
2014-06-02 12:09:09 -0600

    Fixed Angular and changed background
    Finalized webpage; Fixed Angularjs issue and changed background to a
    texture.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,21 +4,30 @@
 	var profileApp = angular.module('profileApp', ['ui.router'])
 	profileApp.config(function($stateProvider, $urlRouterProvider) {
 
-	$urlRouterProvider.otherwise("/about")
+		$urlRouterProvider.otherwise(function ($injector, $location) {
+	        debugger
+	        return ("/about");
+	     });
 
-	$stateProvider
-	.state('about', {
-		url: "/about",
-		templateUrl: "templates/about.html"
-
-	})
-	.state('education', {
-		url: '/education',
-		templateUrl: 'templates/education.html'
-	})
-	.state('interests', {
-		url: '/interests',
-		templateUrl: 'templates/interests.html'
-	})
-});
+		$stateProvider
+		.state('about', {
+			url: "/about",
+			templateUrl: "templates/about.html"
+		})
+		.state('education', {
+			url: '/education',
+			templateUrl: 'templates/education.html'
+		})
+		.state('interests', {
+			url: '/interests',
+			templateUrl: 'templates/interests.html'
+		})
+	});
+	setTimeout(function(){
+		var oldHash = window.location.hash
+		console.log('manually triggering a hash change event in a hacky way')
+		window.location.hash = '#asdf';
+		window.location.hash = oldHash;
+	},1000)
 })();
+
